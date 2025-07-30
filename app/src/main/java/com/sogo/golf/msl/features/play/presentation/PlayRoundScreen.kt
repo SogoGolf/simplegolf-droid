d4cbba4d452c88ff0ca2bf64317de90846716432
mfg-aj <techsupport@myfirstgym.com.au>
2025-07-30 17:49:19 +1000

    upd

--- app/src/main/java/com/sogo/golf/msl/features/play/presentation/PlayRoundScreen.kt
+++ app/src/main/java/com/sogo/golf/msl/features/play/presentation/PlayRoundScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
@@ -62,7 +63,7 @@ private fun Screen4Portrait(
     }
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().navigationBarsPadding()
     ) {
         // HoleHeader as top bar
         HoleHeader(
